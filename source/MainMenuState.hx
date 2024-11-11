7bd032cffc0d782c131eadae2d2a1a12836596ac
Mini Raposa Gamer <115371844+MiniRaposaGAMER@users.noreply.github.com>
2024-11-11 09:06:17 -0300

    galerinha

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -31,12 +31,8 @@ class MainMenuState extends MusicBeatState
 	var debugKeys:Array<FlxKey>;
 
 	var optionShit:Array<String> = [
-		'story_mode',
 		'freeplay',
-		#if MODS_ALLOWED 'mods', #end
-		#if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
-		#if !switch 'donate', #end
 		'options'
 	];
 
