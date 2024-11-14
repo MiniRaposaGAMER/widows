12c3950199ccacfceaa34678248c25b885edb057
Mini Raposa Gamer <115371844+MiniRaposaGAMER@users.noreply.github.com>
2024-11-14 17:07:24 -0300

    teste irmão
    meu pau na sua mão

--- source/mikolka/vslice/charSelect/pslice/ModSelector.hx
+++ source/mikolka/vslice/charSelect/pslice/ModSelector.hx
@@ -50,7 +50,7 @@ class ModSelector extends FlxTypedSpriteGroup<FlxSprite> {
             if (directories[curDirectory] == null || directories[curDirectory].length < 1){
                 ModsHelper.loadModDir("");
                 var nxtArrow = directories.length==1 ? '  ' : '=>';
-                directoryTxt.text = '  No Mod Directory Loaded $nxtArrow';
+                directoryTxt.text = '';
             }
             else
             {
